9b274bb9e350c0baef91c7bb4d09c7daceeaa69b
niklas <niklas>
2006-01-17 19:57:51 +0000

    Changed into two examples

--- test.c
+++ test.c
@@ -4,7 +4,7 @@
 		Test app for the parameterization model lib
 
 		$Source: /home/nkarlsson/usr/cvsroot/cparamlib/Attic/test.c,v $
-		$Author: niklas $ $Date: 2006/01/17 19:57:14 $ $Revision: 1.3 $
+		$Author: niklas $ $Date: 2006/01/17 19:57:51 $ $Revision: 1.4 $
 */
 
 #include <stdio.h>
@@ -32,7 +32,7 @@ int main(void) {
 				/* Example 2: total flux */
 				printf("Example 2: total flux");
 				f = flux(ID_GAMMA, E, Pp);
-				printf("total flux(Tp=512TeV) = %10e\n", f)
+				printf("total flux(Tp=512TeV) = %10e\n", f);
 
 				return 0;
 }
