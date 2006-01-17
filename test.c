f625218ac4c715388bffee19fa53cfd9a3cea298
niklas <niklas>
2006-01-17 19:57:14 +0000

    Changed into two examples

--- test.c
+++ test.c
@@ -4,7 +4,7 @@
 		Test app for the parameterization model lib
 
 		$Source: /home/nkarlsson/usr/cvsroot/cparamlib/Attic/test.c,v $
-		$Author: niklas $ $Date: 2006/01/17 19:53:45 $ $Revision: 1.2 $
+		$Author: niklas $ $Date: 2006/01/17 19:57:14 $ $Revision: 1.3 $
 */
 
 #include <stdio.h>
@@ -14,21 +14,25 @@
 int main(void) {
 				double Tp, Pp, E;
 				double f;
-				double a[8];
+				double a[9];
 				int i;
 
 				Tp = 512000.0;
 				Pp = 512000.938;
 				E = 1.0e2;
 
-				gamma_param_diff(Pp, a);
-
+				/* Example 1: calculate non-diff params and flux */
+				printf("Example 1: non-diff");
+				gamma_param_nd(Pp, a);
 				for (i = 0; i < 8; i++)
 								printf("a[%d] = %10e\n", i, a[i]);
+				f = flux_nd(ID_GAMMA, E, Tp, a);
+				printf("flux_nd(Tp=512TeV) = %10e\n", f);
 
-				f = flux_diff(ID_GAMMA, E, Tp, a);
-
-				printf("flux_diff(Tp=512TeV) = %10e\n", f);
+				/* Example 2: total flux */
+				printf("Example 2: total flux");
+				f = flux(ID_GAMMA, E, Pp);
+				printf("total flux(Tp=512TeV) = %10e\n", f)
 
 				return 0;
 }
