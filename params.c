4baea723fc8d7b7431b1d8f0bec18ff12ac8bfed
niklas <niklas>
2006-01-28 21:38:07 +0000

    Changed to so onecan run easier for different particles

--- params.c
+++ params.c
@@ -5,7 +5,7 @@
 		Calculates parameters as functions of Tp
 
 		$Source: /home/nkarlsson/usr/cvsroot/cparamlib/Attic/params.c,v $
-		$Author: niklas $ $Date: 2006/01/27 23:20:01 $ $Revision: 1.3 $
+		$Author: niklas $ $Date: 2006/01/28 21:38:07 $ $Revision: 1.4 $
 */
 
 #include <stdio.h>
@@ -20,19 +20,27 @@ double Tp_list[43] = {512., 362., 256., 181., 128., 90.5, 64., 45.3, 32.,
 																						1.95e-3, 1.38e-3, 0.98e-3, 0.82e-3, 0.69e-3,
 																						0.58e-3, 0.488e-3};
 
-void nondiff(void) {
+char *filenames[3][4] = {{"GammaNonDiff.dat", "GammaDiffDiss.dat", "GammaDelta1232.dat", "GammaDelta1600.dat"},
+																									{"ElecNonDiff.dat", "ElecDiffDiss.dat", "ElecDelta1232.dat", "ElecDelta1600.dat"},
+																									{"PosiNonDiff.dat", "PosiDiffDiss.dat", "PosiDelta1232.dat", "PosiDelta1600.dat"}};
+
+void nondiff(int p) {
 				double Tp, Pp;
 				double f;
 				double a[9];
 				int i, j;
 				FILE *fp;
 
-				fp = fopen("GammaNonDiff.dat", "w");
-				printf("results written to file!\n");
+				fp = fopen(filenames[p][0], "w");
 				for (i = 0; i < 43; i++) {
 								Tp = Tp_list[i]*1000.0;
 								Pp = sqrt(Tp*Tp + 2.0*Tp*m_p);
-								gamma_param_nd(Pp, a);
+								if (p == 0)
+												gamma_param_nd(Pp, a);
+								else if (p == 1)
+												elec_param_nd(Pp, a);
+								else if (p == 2)
+												posi_param_nd(Pp, a);
 								fprintf(fp, "%f ", Tp_list[i]);
 								for (j = 0; j < 9; j++) {
 												fprintf(fp, "%10e", a[j]);
@@ -45,19 +53,23 @@ void nondiff(void) {
 				fclose(fp);
 }
 
-void diffdiss(void) {
+void diffdiss(int p) {
 				double Tp, Pp;
 				double f;
 				double b[8];
 				int i, j;
 				FILE *fp;
 
-				fp = fopen("GammaDiffDiss.dat", "w");
-				printf("results written to file!\n");
+				fp = fopen(filenames[p][1], "w");
 				for (i = 0; i < 37; i++) {
 								Tp = Tp_list[i]*1000.0;
 								Pp = sqrt(Tp*Tp + 2.0*Tp*m_p);
-								gamma_param_diff(Pp, b);
+								if (p == 0)
+												gamma_param_diff(Pp, b);
+								else if (p == 1)
+												elec_param_diff(Pp, b);
+								else if (p == 2)
+												posi_param_diff(Pp, b);
 								fprintf(fp, "%f ", Tp_list[i]);
 								for (j = 0; j < 8; j++) {
 												fprintf(fp, "%10e", b[j]);
@@ -70,19 +82,23 @@ void diffdiss(void) {
 				fclose(fp);
 }
 
-void delta1232(void) {
+void delta1232(int p) {
 				double Tp, Pp;
 				double f;
 				double c[5];
 				int i, j;
 				FILE *fp;
 
-				fp = fopen("GammaDelta1232.dat", "w");
-				printf("results written to file!\n");
+				fp = fopen(filenames[p][2], "w");
 				for (i = 36; i < 43; i++) {
 								Tp = Tp_list[i]*1000.0;
 								Pp = sqrt(Tp*Tp + 2.0*Tp*m_p);
-								gamma_param_delta(Pp, c);
+								if (p == 0)
+												gamma_param_delta(Pp, c);
+								else if (p == 1)
+												elec_param_delta(Pp, c);
+								else if (p == 2)
+												posi_param_delta(Pp, c);
 								fprintf(fp, "%f ", Tp_list[i]);
 								for (j = 0; j < 5; j++) {
 												fprintf(fp, "%10e", c[j]);
@@ -95,19 +111,23 @@ void delta1232(void) {
 				fclose(fp);
 }
 
-void delta1600(void) {
+void delta1600(int p) {
 				double Tp, Pp;
 				double f;
 				double d[5];
 				int i, j;
 				FILE *fp;
 
-				fp = fopen("GammaDelta1600.dat", "w");
-				printf("results written to file!\n");
+				fp = fopen(filenames[p][3], "w");
 				for (i = 35; i < 41; i++) {
 								Tp = Tp_list[i]*1000.0;
 								Pp = sqrt(Tp*Tp + 2.0*Tp*m_p);
-								gamma_param_res(Pp, d);
+								if (p == 0)
+												gamma_param_res(Pp, d);
+								else if (p == 1)
+												elec_param_res(Pp, d);
+								else if (p == 2)
+												posi_param_res(Pp, d);
 								fprintf(fp, "%f ", Tp_list[i]);
 								for (j = 0; j < 5; j++) {
 												fprintf(fp, "%10e", d[j]);
@@ -121,10 +141,10 @@ void delta1600(void) {
 }
 
 int main(void) {
-				nondiff();
-				diffdiss();
-				delta1232();
-				delta1600();
+				nondiff(2);
+				diffdiss(2);
+				delta1232(2);
+				delta1600(2);
 
 				return 0;
 }
