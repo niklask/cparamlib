22ea8e6faa69d968e34d9e06cfa3cf4cffb2b893
niklas <niklas>
2006-11-25 02:17:15 +0000

    Updating files for autogeneration of python wrapper around cparamlib

---
+++ python/__init__.py
@@ -0,0 +1,8 @@
+#
+# cparamlib python package
+#
+# $Source: /home/nkarlsson/usr/cvsroot/cparamlib/python/__init__.py,v $
+# $Author: niklas $ $Date: 2006/11/25 02:17:15 $ $Revision: 1.1 $
+#
+
+__all__ = ["ParamModel"]
