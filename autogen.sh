07681328f556564353f5ca049d1fff59ab58758c
niklas <niklas>
2007-03-29 04:06:07 +0000

    *** empty log message ***

--- autogen.sh
+++ autogen.sh
@@ -6,6 +6,6 @@ if [ ! -d config ]; then
 		echo "mkdir config"
 		mkdir config
 fi
-autoreconf --force --install -I config -I m4 && exit 0
+autoreconf --force --install -I config && exit 0
 
 exit 1
